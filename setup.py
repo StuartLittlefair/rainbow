0bcfebc9eb8745ade6db007bd6cec77bd91ed981
Stuart Littlefair <s.littlefair@shef.ac.uk>
2014-08-22 10:29:38 +0100

    cleaning up installation

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(name='rainbow',
     author = "S. Littlefair",
     url = "https://github.com/StuartLittlefair/rainbow",
     author_email = "s.littlefair@shef.ac.uk",
+    zip_safe = False,
     install_requires=[
         'numpy',
         'triangle_plot'
